b57212f4d0012adde88f7134ac71cb3aa9d18f8f
Diego Cristóbal Herreros <diego.cristobal@teamcmp.com>
2023-06-11 17:00:14 +0200

    Fix: format main

--- infra/main.tf
+++ infra/main.tf
@@ -17,7 +17,7 @@ resource "civo_kubernetes_cluster" "demo-cluster" {
   firewall_id  = civo_firewall.firewall.id
   cluster_type = local.cluster.type
   applications = try(local.cluster.applications, "")
-  cni = try(local.cluster.cni, "")
+  cni          = try(local.cluster.cni, "")
   pools {
     label      = local.cluster.label-nodes
     size       = element(data.civo_size.medium.sizes, 0).name
