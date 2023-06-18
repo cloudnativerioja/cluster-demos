cdb412db5731bd141f2dc9ed67b8b8beb5af5cd8
Diego Cristóbal Herreros <diego.cristobal@teamcmp.com>
2023-06-18 21:24:48 +0200

    Fix: format terraform code

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-      bucket = "tf-state-remote-civo"
-      key = "terraform-state/demo-civo.tfstate"
-      region  = "us-east-1"
-    }
+  backend "s3" {
+    bucket = "tf-state-remote-civo"
+    key    = "terraform-state/demo-civo.tfstate"
+    region = "us-east-1"
+  }
 
   required_providers {
     civo = {
