baa22acda449171618eb2399c71faec41b62142b
Diego Cristóbal Herreros <diego.cristobal@teamcmp.com>
2023-07-16 08:27:21 +0200

    Fix: remove some lint problems and helm provider

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,20 +2,3 @@ provider "civo" {
   token  = var.civo_token
   region = local.cluster.region
 }
-
-provider "helm" {
-  kubernetes {
-    config_path = local_file.kubeconfig.filename
-    exec {
-      api_version = "client.authentication.k8s.io/v1"
-      command     = "civo"
-      args = [
-        "kubernetes",
-        "config",
-        civo_kubernetes_cluster.demo-cluster.name,
-        "--save",
-        "--override",
-      ]
-    }
-  }
-}
